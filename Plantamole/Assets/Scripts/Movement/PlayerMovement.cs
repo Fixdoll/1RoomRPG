774b4ab2c8de175b73d0c594fb40d64b7109c533
Fixdoll <fixdoll1@gmail.com>
2018-02-03 22:07:16 +0300

    Draft gameplay
    movement and such

---
+++ Plantamole/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public int speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        var x = Input.GetAxis("Horizontal") * speed / 100;
+        var y = Input.GetAxis("Vertical") * speed / 100;
+        if (x > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if(x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        transform.Translate(x, y, 0);
+
+
+    }
+}
