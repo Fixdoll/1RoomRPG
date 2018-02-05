5010c854412e0a0d0db563d5a4d777c0f36e5f67
Fixdoll <fixdoll1@gmail.com>
2018-02-05 15:30:30 +0300

    More into Selection

--- Plantamole/Assets/Code/GameController.cs
+++ Plantamole/Assets/Code/GameController.cs
@@ -3,9 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameController : MonoBehaviour {
-    
+public class GameController : MonoBehaviour{
+
+    public List<Tile> xTiles;
+    public static List<Tile> Tiles = new List<Tile>();
+
+    private void Start()
+    {
+        foreach (Tile xt in xTiles)
+        {
+            Tiles.Add(xt);
+        }
+    }
+
+
     public static Vector3 GetTruePos (Vector3 pos) {
         return new Vector3(pos.x, pos.y, pos.y - 10f);
+    }
+
+    public static bool HighlightPresent()
+    {
+        foreach(Tile t in Tiles)
+        {
+            if (t.Selected) return true;
+        }
+        return false;
     }
 }
--- Plantamole/Assets/Code/Tile.cs
+++ Plantamole/Assets/Code/Tile.cs
@@ -22,7 +22,13 @@ public class Tile : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player" && !GameController.HighlightPresent())
+            Selected = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !Selected && !GameController.HighlightPresent())
             Selected = true;
     }
 
